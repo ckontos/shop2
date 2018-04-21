16ab2faedbd79f7da05a9e6763b22a6426c91275
Dimitri Kontos <ckontos@rocketmail.com>
2018-04-21 00:25:34 +0000

    up to date

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,24 @@
-window.onload = function() {
+// window.onload = function() {
 
- 
-  // run a call to delete the articles
-  $.ajax({
-      method: "DELETE",
-      url: "/review/deleteAll" 
-    }).done(function() {
-      $.ajax({// then run the scrape
-        method: "GET",
-        url: "/scrape"
-      }).done(function(data) {
-      $.ajax({// then run the scrape
-        method: "GET",
-        url: "/scrape2"
-      })
-      });
-    
-    });
 
-}
+    //   // run a call to delete the articles
+    //   $.ajax({
+    //       method: "DELETE",
+    //       url: "/review/deleteAll" 
+    //     }).done(function() {
+    //       $.ajax({// then run the scrape
+    //         method: "GET",
+    //         url: "/scrape"
+    //       }).done(function(data) {
+    //       $.ajax({// then run the scrape
+    //         method: "GET",
+    //         url: "/scrape2"
+    //       })
+    //       });
+
+    //     });
+
+    // }
 
 
 $("#scheduleNowBtn").on("click", function() {
